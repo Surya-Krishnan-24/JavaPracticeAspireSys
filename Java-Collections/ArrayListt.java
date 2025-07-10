7487986c7c55146607e465420fac0a767ae67518
Surya-Krishnan-24 <surya.krishnan@aspiresys.com>
2025-07-10 16:28:20 +0530

    Update ArrayListt.java

--- Java-Collections/ArrayListt.java
+++ Java-Collections/ArrayListt.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListt {
+    
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
 
